789a29ad006c42264dff335b8a33678d6ab5eb7e
Anupam Sharma <sharma.anupam106@gmail.com>
2021-01-14 22:55:40 -0500

    run_server.py added to start the server

---
+++ TvShuffleForPlex/run_server.py
@@ -0,0 +1,40 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import manage
+import sys
+
+
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "TestService"
+    _svc_display_name_ = "Test Service"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_,''))
+        self.main()
+
+    def main(self):
+        m_list = [
+            "manage.py",
+            "runserver",
+            "localhost:5000"
+        ]
+        sys.argv = m_list
+        manage.main()
+
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
--- run_server.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("./TvShuffleForPlex/manage.py runserver localhost:5000")
